d01f71b887a3b2eaa4df63b969c3d48632551492
Antonio Paya <antonioalfa22@gmail.com>
2018-04-12 09:54:34 +0200

    Add modules

--- app.js
+++ app.js
@@ -8,4 +8,22 @@
  * @author Pablo Diaz
  */
 
+//==========MODULOS===============
+var express = require('express');
+var app = express();
 
+var mongo = require('mongodb');
+var gestorDB = require("./modules/gestorDB.js");
+var swig = require('swig');
+var fileUpload = require('express-fileupload');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var fs = require('fs');
+var https = require('https');
+var crypto = require('crypto');
+
+//==========VARIABLES===============
+app.set('port', 8081);
+app.set('db', 'mongodb://sdi:EIISDI2018$@ds245478.mlab.com:45478/redsocial');
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
--- modules/gestorDB.js
+++ modules/gestorDB.js
@@ -6,3 +6,45 @@
  * @author Antonio Paya
  * @author Pablo Diaz
  */
+ module.exports = {
+    mongo: null,
+    app: null,
+    init: function (app, mongo) {
+        this.mongo = mongo;
+        this.app = app;
+    },
+    addUsuario: function (usuario, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collecion = db.collection('usuarios');
+                collecion.insert(usuario, function (err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    getUsuarios: function (criterio, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                var collecion = db.collection('usuarios');
+                collecion.find(criterio).toArray(function (err, usuarios) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(usuarios);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+};
