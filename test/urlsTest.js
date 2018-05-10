9ed46134155ffc879b2399703c6e7fb97558f234
Antonio Paya <antonioalfa22@gmail.com>
2018-05-10 12:08:47 +0200

    Update test

---
+++ test/urlsTest.js
@@ -0,0 +1,51 @@
+var assert = require("chai").assert;
+var app = require("../app");
+var request = require('request');
+
+describe("Test de comprobacion de URLS: ", function() {
+
+    describe ('Login & Registro', function() {
+        it('Test login - "/" devuelve pagina Login', function(){
+            request('http://localhost:8081/', function(error, response, body) {
+                expect(body).to.equal('Login');
+            });
+        });
+        it('Test login - "/users/lista-amigos devuelve pagina Login', function(){
+            request('http://localhost:8081/users/lista-amigos', function(error, response, body) {
+                expect(body).to.equal('Login');
+            });
+        });
+        it('Test login - "/panel devuelve pagina Login', function(){
+            request('http://localhost:8081/panel', function(error, response, body) {
+                expect(body).to.equal('Login');
+            });
+        });
+        it('Test registro - "/registro devuelve pagina Registro', function(){
+            request('http://localhost:8081/registro', function(error, response, body) {
+                expect(body).to.equal('Regístrate');
+            });
+        });
+    });
+
+});
+
+describe("Test de comprobacion de errores: ", function() {
+
+    describe ('Error 404', function() {
+        it('Test error 404 - "/absf" devuelve pagina error 404', function(){
+            request('http://localhost:8081/absf', function(error, response, body) {
+                expect(response.statusCode).to.equal(404);
+                expect(body).to.equal('Error 404');
+            });
+        });
+    });
+
+    describe ('Error 404', function() {
+        it('Test error 404 - "/absf" devuelve status 404', function(){
+            request('http://localhost:8081/absf', function(error, response, body) {
+                expect(response.statusCode).to.equal(404);
+            });
+        });
+    });
+
+});
