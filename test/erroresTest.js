b764f5f9b6668cfe7040a77d510041c297e88f9c
Antonio Paya <antonioalfa22@gmail.com>
2018-05-10 12:52:33 +0200

    Update test

---
+++ test/erroresTest.js
@@ -0,0 +1,23 @@
+var assert = require("chai").assert;
+var app = require("../app");
+var request = require('request');
+
+describe("Test de comprobacion de errores: ", function() {
+
+    describe ('Error 404', function() {
+        it('Test error 404 - "/absf" devuelve pagina error 404', function(){
+            request('http://localhost:8081/absf', function(error, response, body) {
+                expect(body).to.equal('Error 404');
+            });
+        });
+    });
+
+    describe ('Error 404', function() {
+        it('Test error 404 - "/absf" devuelve status 404', function(){
+            request('http://localhost:8081/absf', function(error, response, body) {
+                expect(response.statusCode).to.equal(404);
+            });
+        });
+    });
+
+});
--- test/urlsTest.js
+++ test/urlsTest.js
@@ -20,25 +20,9 @@ describe("Test de comprobacion de URLS: ", function() {
                 expect(body).to.equal('Login');
             });
         });
-        
-    });
-
-});
-
-describe("Test de comprobacion de errores: ", function() {
-
-    describe ('Error 404', function() {
-        it('Test error 404 - "/absf" devuelve pagina error 404', function(){
-            request('http://localhost:8081/absf', function(error, response, body) {
-                expect(body).to.equal('Error 404');
-            });
-        });
-    });
-
-    describe ('Error 404', function() {
-        it('Test error 404 - "/absf" devuelve status 404', function(){
-            request('http://localhost:8081/absf', function(error, response, body) {
-                expect(response.statusCode).to.equal(404);
+        it('Test registro - "/registro devuelve pagina Registro', function(){
+            request('http://localhost:8081/registro', function(error, response, body) {
+                expect(body).to.equal('Registro');
             });
         });
     });
