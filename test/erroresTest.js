de1306f8796d5e52ac8f244cad2d34711723f77b
Antonio Paya <antonioalfa22@gmail.com>
2018-05-10 13:12:10 +0200

    Update travis

--- test/erroresTest.js
+++ test/erroresTest.js
@@ -6,6 +6,14 @@ var expect = require("chai").expect;
 describe("Test de comprobacion de errores: ", function() {
 
     describe ('Error 404', function() {
+        it('Test error 404 - "/absf" devuelve pagina error 404', function(){
+            request('http://localhost:8081/absf', function(error, response, body) {
+                expect(body).to.equal('Error 404 Page not found');
+            });
+        });
+    });
+
+    describe ('Error 404', function() {
         it('Test error 404 - "/absf" devuelve status 404', function(){
             request('http://localhost:8081/absf', function(error, response, body) {
                 expect(response.statusCode).to.equal(404);
